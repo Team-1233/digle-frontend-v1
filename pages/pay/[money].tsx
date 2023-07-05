1ba91aed99eb80eba9d7e929305549629be58d8a
woosang <woosang3078@hanmail.net>
2023-07-05 22:22:42 +0900

    feat: 동적 라우팅 페이지 추가

--- pages/pay/[money].tsx
+++ pages/pay/[money].tsx
@@ -1,8 +1,11 @@
+// pages/pay/[money].tsx
+
 import { useEffect, useRef, useState } from "react";
 import { loadPaymentWidget } from "@tosspayments/payment-widget-sdk";
 import { nanoid } from "nanoid";
 import { useAsync } from "react-use";
-import * as S from "../styles/index";
+import { useRouter } from "next/router";
+import * as S from "../../styles/index";
 
 const clientKey = "test_ck_5GePWvyJnrKEeBowDGb3gLzN97Eo";
 const customoerKey = "payments";
@@ -10,7 +13,9 @@ const customoerKey = "payments";
 const TossPay = () => {
   const paymentWidgetRef = useRef<any>(null);
   const paymentMethodsWidgetRef = useRef<any>(null);
-  const [price, setPrice] = useState(100);
+  const [price, setPrice] = useState<number>(100);
+  const router = useRouter();
+  const { money } = router.query;
 
   useAsync(async () => {
     const paymentWidget = await loadPaymentWidget(clientKey, customoerKey);
@@ -37,29 +42,35 @@ const TossPay = () => {
     );
   }, [price]);
 
+  useEffect(() => {
+    if (money) {
+      setPrice(parseInt(money as string));
+    }
+  }, [money]);
+
   const handleClick = async () => {
     const paymentWidget = paymentWidgetRef.current;
 
     try {
-        await paymentWidget?.requestPayment({
-            orderId: nanoid(),
-            orderName: '디글 후원',
-            customerName: '조우상',
-            customerEmail: 'woosang3078@gmail.com',
-            successUrl: `${window.location.origin}/success`,
-            failUrl: `${window.location.origin}/fail`
-        })
+      await paymentWidget?.requestPayment({
+        orderId: nanoid(),
+        orderName: '디글 후원',
+        customerName: '조우상',
+        customerEmail: 'woosang3078@gmail.com',
+        successUrl: `${window.location.origin}/success`,
+        failUrl: `${window.location.origin}/fail`
+      });
     } catch (error) {
-        console.error(error)
+      console.error(error);
     }
-  }
+  };
 
   return (
     <div>
       <S.PaymentWidget id="payment-widget" />
       <S.Footer>
         <S.PriceInfo>
-          후원 금액 : {price}원
+          후원 금액: {price}원
         </S.PriceInfo>
         <S.PurchaseBtn onClick={handleClick}>디글과 함께 후원하기</S.PurchaseBtn>
       </S.Footer>
