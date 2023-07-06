79a0ccd775291fa1102f922821b489d885c41cae
woosang <woosang3078@hanmail.net>
2023-07-06 09:17:37 +0900

    fix: 금액 오류 수정

--- pages/pay/[money].tsx
+++ pages/pay/[money].tsx
@@ -1,21 +1,27 @@
-// pages/pay/[money].tsx
-
 import { useEffect, useRef, useState } from "react";
 import { loadPaymentWidget } from "@tosspayments/payment-widget-sdk";
 import { nanoid } from "nanoid";
 import { useAsync } from "react-use";
 import { useRouter } from "next/router";
 import * as S from "../../styles/index";
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  GetStaticPaths,
+  GetStaticProps,
+  NextPage,
+} from "next";
+
+interface PageProps {
+  price: number;
+}
 
 const clientKey = "test_ck_5GePWvyJnrKEeBowDGb3gLzN97Eo";
 const customoerKey = "payments";
 
-const TossPay = () => {
+const TossPay: NextPage<PageProps> = ({ price }: PageProps) => {
   const paymentWidgetRef = useRef<any>(null);
   const paymentMethodsWidgetRef = useRef<any>(null);
-  const [price, setPrice] = useState<number>(100);
-  const router = useRouter();
-  const { money } = router.query;
 
   useAsync(async () => {
     const paymentWidget = await loadPaymentWidget(clientKey, customoerKey);
@@ -43,10 +49,11 @@ const TossPay = () => {
   }, [price]);
 
   useEffect(() => {
-    if (money) {
-      setPrice(parseInt(money as string));
-    }
-  }, [money]);
+    // if (query.price) {
+    //   const parsedPrice = parseInt(query.price as string);
+    //   setPrice(parsedPrice);
+    // }
+  }, []);
 
   const handleClick = async () => {
     const paymentWidget = paymentWidgetRef.current;
@@ -54,11 +61,12 @@ const TossPay = () => {
     try {
       await paymentWidget?.requestPayment({
         orderId: nanoid(),
-        orderName: '디글 후원',
-        customerName: '조우상',
-        customerEmail: 'woosang3078@gmail.com',
+        orderName: "디글 후원",
+        customerName: "조우상",
+        customerEmail: "woosang3078@gmail.com",
+        amount: price,
         successUrl: `${window.location.origin}/success`,
-        failUrl: `${window.location.origin}/fail`
+        failUrl: `${window.location.origin}/fail`,
       });
     } catch (error) {
       console.error(error);
@@ -69,13 +77,19 @@ const TossPay = () => {
     <div>
       <S.PaymentWidget id="payment-widget" />
       <S.Footer>
-        <S.PriceInfo>
-          후원 금액: {price}원
-        </S.PriceInfo>
-        <S.PurchaseBtn onClick={handleClick}>디글과 함께 후원하기</S.PurchaseBtn>
+        <S.PriceInfo>후원 금액: {price}원</S.PriceInfo>
+        <S.PurchaseBtn onClick={handleClick}>
+          디글과 함께 후원하기
+        </S.PurchaseBtn>
       </S.Footer>
     </div>
   );
+};
+
+export const getServerSideProps = (ctx: GetServerSidePropsContext) => {
+  const queryMoney = ctx.query.money as string;
+
+  return { props: { price: parseInt(queryMoney) || 0 } };
 };
 
 export default TossPay;
